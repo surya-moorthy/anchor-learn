8d32a4f8e491cb5ac15a922b91e4ccc54bca21b2
surya-moorthy <tssuryamoorthy20092005@gmail.com>
2025-06-26 21:36:01 +0530

    worked on creating an account

--- programs/learn/src/lib.rs
+++ programs/learn/src/lib.rs
@@ -4,6 +4,8 @@ declare_id!("CAVZT8xhXfRKhZSBEJ884dvjpDsTaDZjjT3MxXNumbwj");
 
 #[program]
 pub mod learn {
+    use anchor_lang::system_program::{CreateAccount,create_account};
+
     use crate::instruction::HelloWorld;
 
     use super::*;
@@ -14,7 +16,42 @@ pub mod learn {
         msg!("this is a basic program which has a programid : {}",&id());
         Ok(())
     } 
+
+    pub fn create_system_account(ctx : Context<CreateSystemAccount>) -> Result<()> {
+        msg!("System program is invoked, lets create an account");
+
+        msg!("The new account public key is : {}", &ctx.accounts.new_account.key());
+
+        let lamports = (Rent::get()?).minimum_balance(0);
+
+        let create_sytems_account = CpiContext::new(
+             ctx.accounts.system_program.to_account_info(),
+             CreateAccount {
+                from: ctx.accounts.signer.to_account_info(),
+                to: ctx.accounts.new_account.to_account_info()
+             }
+        );
+        
+        create_account(create_sytems_account, lamports, 0, &ctx.accounts.system_program.key())?;
+
+        msg!("account create successfully");
+
+        Ok(())
+    }
+
+
 }
 
 #[derive(Accounts)]
 pub struct Hello {}
+
+#[derive(Accounts)]
+pub struct CreateSystemAccount<'info> {
+    #[account(mut)]
+    pub signer : Signer<'info>,
+    
+    #[account(mut)]
+    pub new_account : Signer<'info>,
+
+    pub system_program : Program<'info, System>
+}
--- tests/learn.ts
+++ tests/learn.ts
@@ -1,14 +1,34 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Learn } from "../target/types/learn";
+import { assert } from "chai";
 
 describe("learn", () => {
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
-  const program = anchor.workspace.Learn as Program<Learn>;
+  const program = anchor.workspace.Learn as Program<Learn>; 
+  const connection = provider.connection;
+  const wallet = provider.wallet as anchor.Wallet;
 
   it("Say hello", async () => {
     await program.methods.helloWorld().accounts({}).rpc();
   })
+
+  it("create new account", async () => {
+    const newKeypair = anchor.web3.Keypair.generate();
+
+    await program.methods.createSystemAccount().accounts({
+        signer : wallet.publicKey,
+        newAccount: newKeypair.publicKey     
+    }).signers([newKeypair]).rpc();
+
+    const lamports = await connection.getMinimumBalanceForRentExemption(0);
+
+    const accountInfo = await connection.getAccountInfo(newKeypair.publicKey);
+    assert(accountInfo.lamports === lamports);
+  })
+
+  
+  
 });
